2fc7d42e7fdefd2240ffb5ec2bd8a27b393f6e76
Carlos Rosario <carlosrosario2k@gmail.com>
2020-07-10 23:40:08 -0400

    def initialized instructions and ingredients

--- lib/cocktail.rb
+++ lib/cocktail.rb
@@ -1,13 +1,23 @@
 class Cocktail
-  attr_accessor :name
+  attr_accessor :name, :instructions, :drinks, :ingredients
   @@all = []
-  def initialize
+  def initialize(name, instructions, ingredients)
     @name = name
+    @ingredients = ingredients
+    @instructions = instructions
     @@all << self
   end
- def self.all
+
+  def self.all
     @@all
   end
 def drink_name
   puts self.name
+end
+def self.instructions
+  puts @instructions 
+end
+def self.ingredients
+  @ingredients
+end
 end
