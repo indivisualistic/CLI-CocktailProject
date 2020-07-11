98999bf28c219e4f9c44a6acc169a05bc1b16281
Carlos Rosario <carlosrosario2k@gmail.com>
2020-07-10 23:44:12 -0400

    def user input and print cocktail

--- lib/cli.rb
+++ lib/cli.rb
@@ -11,6 +11,17 @@ class CLI
  
     puts "How about the drink of the day?"
   end
+  if user_input == "1"
+    
+    @api.get_info
+    @cocktail = Cocktail.all[i]
+    puts "Drink Name:"
+    puts Cocktail.all[i].name
+    
+  elsif user_input == "2"
+    
+    
+  end
 
   
   
