a25e7db0f78984b83199fabafec5e0763abbdf2c
Carlos Rosario <carlosrosario2k@gmail.com>
2020-07-10 23:33:04 -0400

    defined class cli and initialized

---
+++ lib/cli.rb
@@ -0,0 +1,33 @@
+require 'pry'
+class CLI
+  attr_accessor :user_input, :cocktail
+  def initialize
+  @user_input = user_input
+  @api = API.new 
+  end
+  
+  
+  
+  
+
+  
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
